050e5c404e442bdd8b0879b0c82325d493596e12
ابوالفضل بابانظری <odaat.iaath@gmail.com>
2020-12-20 14:04:19 +0330

    There was a problem in closing sniffer and server after closing terminal

--- launch.sh
+++ launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 make clean
 make
-sudo python http_server.py 127.1.1.0 80 >/dev/null 2>&1 &
+python http_server.py 127.1.1.0 80 >/dev/null 2>&1 &
 
 for((i=0; i<"$1"; i++))
 do
@@ -9,13 +9,13 @@ do
     python http_client.py -a -m --delay "$2" 127.1.1.0 80 >/dev/null 2>&1 &
 done
 
-sudo ./sniffer lo &
+./sniffer lo &
 
 function handle_sigint()
 {
     for process in $(jobs -p)
     do
-        sudo kill $process
+        kill $process
     done
 }
 
